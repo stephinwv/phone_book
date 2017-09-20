75ab08e184c2ee08cce0c80e5800e7a78c290c10
stephinwv <thekingdomseeker@aol.com>
2017-09-20 14:24:38 -0400

    I have a working search function, update function and delete all function. Now on to CSS.

--- app.rb
+++ app.rb
@@ -16,33 +16,52 @@ db_params = {
 db = PG::Connection.new(db_params)
 
 get '/' do
-phonebook = db.exec("Select * From phonebook")
-erb :selection, locals: {phonebook: phonebook}
+	phonebook = db.exec("Select * From phonebook")
+	erb :selection, locals: {phonebook: phonebook}
 end
 post '/selection' do
-first_name = params[:first_name]
-last_name = params[:last_name]
-address = params[:address]
-city = params[:city]
-state = params[:state]
-zip = params[:zip]
-phone = params[:phone]
-email = params[:email]
+
+	first_name = params[:first_name]
+	last_name = params[:last_name]
+	address = params[:address]
+	city = params[:city]
+	state = params[:state]
+	zip = params[:zip]
+	phone = params[:phone]
+	email = params[:email]
 
 
-db.exec("INSERT INTO phonebook(first_name, last_name, address, city, state, zip, phone, email) VALUES('#{first_name}', '#{last_name}', '#{address}', '#{city}', '#{state}', '#{zip}', '#{phone}', '#{email}')");
-
+	db.exec("INSERT INTO phonebook(first_name, last_name, address, city, state, zip, phone, email) VALUES('#{first_name}', '#{last_name}', '#{address}', '#{city}', '#{state}', '#{zip}', '#{phone}', '#{email}')");
+	redirect '/'
 end
-get '/selection'do
-first_name = params[:first_name]
-last_name = params[:last_name]
-address = params[:address]
-city = params[:city]
-state = params[:state]
-zip = params[:zip]
-phone = params[:phone]
-email = params[:email]
-phonebook = params[:phonebook]
+post '/update_column' do
 
-erb :selection, locals:{first_name: first_name, last_name:last_name, address:address, city: city, state: state, zip: zip, phone: phone, email: email}
+   new_data = params[:new_data]
+   old_data = params[:old_data]
+   column = params[:table_column]
+
+   case column
+   when 'col_first_name'
+   	db.exec("UPDATE phonebook SET first_name = '#{new_data}' WHERE first_name = '#{old_data}' ");
+   when 'col_last_name'
+   	db.exec("UPDATE phonebook SET last_name = '#{new_data}' WHERE last_name = '#{old_data}' ");
+   when 'col_address'
+   	db.exec("UPDATE phonebook SET address = '#{new_data}' WHERE street_address = '#{old_data}' ");
+   when 'col_city'
+   	db.exec("UPDATE phonebook SET city = '#{new_data}' WHERE city = '#{old_data}' ");
+   when 'col_state'
+   	db.exec("UPDATE phonebook SET state = '#{new_data}' WHERE state = '#{old_data}' ");
+   when 'col_zip'
+   	db.exec("UPDATE phonebook SET zip = '#{new_data}' WHERE zip = '#{old_data}' ");
+   when 'col_cell'
+   	db.exec("UPDATE phonebook SET phone = '#{new_data}' WHERE phone = '#{old_data}' ");
+   when 'col_home'
+	db.exec("UPDATE phonebook SET email = '#{new_data}' WHERE email = '#{old_data}' ");
+	end
+   redirect '/'
+end
+
+post '/delete_all' do
+	db.exec("TRUNCATE phonebook");
+	redirect '/'
 end
