445eaef476e4bf831493d3aa6efe6dddf5f93bca
stephinwv <thekingdomseeker@aol.com>
2017-09-21 21:02:41 -0400

    Added a delete function for individual entry, added CSS

--- app.rb
+++ app.rb
@@ -40,28 +40,47 @@ post '/update_column' do
    old_data = params[:old_data]
    column = params[:table_column]
 
-   case column
-   when 'col_first_name'
-   	db.exec("UPDATE phonebook SET first_name = '#{new_data}' WHERE first_name = '#{old_data}' ");
-   when 'col_last_name'
-   	db.exec("UPDATE phonebook SET last_name = '#{new_data}' WHERE last_name = '#{old_data}' ");
-   when 'col_address'
-   	db.exec("UPDATE phonebook SET address = '#{new_data}' WHERE street_address = '#{old_data}' ");
-   when 'col_city'
-   	db.exec("UPDATE phonebook SET city = '#{new_data}' WHERE city = '#{old_data}' ");
-   when 'col_state'
-   	db.exec("UPDATE phonebook SET state = '#{new_data}' WHERE state = '#{old_data}' ");
-   when 'col_zip'
-   	db.exec("UPDATE phonebook SET zip = '#{new_data}' WHERE zip = '#{old_data}' ");
-   when 'col_cell'
-   	db.exec("UPDATE phonebook SET phone = '#{new_data}' WHERE phone = '#{old_data}' ");
-   when 'col_home'
-	db.exec("UPDATE phonebook SET email = '#{new_data}' WHERE email = '#{old_data}' ");
+   	case column
+   		when 'col_first_name'
+   			db.exec("UPDATE phonebook SET first_name = '#{new_data}' WHERE first_name = '#{old_data}' ");
+   		when 'col_last_name'
+   			db.exec("UPDATE phonebook SET last_name = '#{new_data}' WHERE last_name = '#{old_data}' ");
+   		when 'col_address'
+   			db.exec("UPDATE phonebook SET address = '#{new_data}' WHERE street_address = '#{old_data}' ");
+   		when 'col_city'
+   			db.exec("UPDATE phonebook SET city = '#{new_data}' WHERE city = '#{old_data}' ");
+   		when 'col_state'
+   			db.exec("UPDATE phonebook SET state = '#{new_data}' WHERE state = '#{old_data}' ");
+   		when 'col_zip'
+   			db.exec("UPDATE phonebook SET zip = '#{new_data}' WHERE zip = '#{old_data}' ");
+   		when 'col_cell'
+   			db.exec("UPDATE phonebook SET phone = '#{new_data}' WHERE phone = '#{old_data}' ");
+   		when 'col_home'
+			db.exec("UPDATE phonebook SET email = '#{new_data}' WHERE email = '#{old_data}' ");
 	end
    redirect '/'
 end
 
-post '/delete_all' do
-	db.exec("TRUNCATE phonebook");
-	redirect '/'
+post "/delete" do
+        deleted = params[:data_to_delete]
+        column = params[:table_column]
+    case column
+   		when 'col_first_name'
+        	db.exec("DELETE FROM phonebook WHERE first_name = '#{deleted}'")
+    	when 'col_last_name'
+       		db.exec("DELETE FROM phonebook WHERE last_name = '#{deleted}'");
+   		when 'col_address'
+        	db.exec("DELETE FROM phonebook WHERE address = '#{deleted}'");
+    	when 'col_city'
+        	db.exec("DELETE FROM phonebook WHERE city = '#{deleted}'");
+    	when 'col_state'
+        	db.exec("DELETE FROM phonebook WHERE state = '#{deleted}'");
+    	when 'col_zip'
+        	db.exec("DELETE FROM phonebook WHERE zip = '#{deleted}'");        
+    	when 'col_phone'
+        	db.exec("DELETE FROM phonebook WHERE phone = '#{deleted}'");
+    	when 'col_email'
+     	db.exec("DELETE FROM phonebook WHERE email = '#{deleted}'");
+    end
+     redirect '/'
 end
