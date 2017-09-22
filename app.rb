071b170516961739f642fe8a6f20e041afc50bf7
stephinwv <thekingdomseeker@aol.com>
2017-09-22 07:04:15 -0400

    Updated config for heroku

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ require 'pg'
 
 
 load './local_env.rb' if File.exists?('./local_env.rb')
-
+heroku params = {
+	username: ENV['username'],
+	password: ENV['password']
 db_params = {
 	host: ENV['host'],
 	port: ENV['port'],
